83aba5391088641f3abbcbab4360f70c08b3da30
Manuel Kienlein <manuel.kienlein@tum.de>
2023-03-08 18:25:22 +0100

    Refactoring

--- test-scripts/test_PSA_02.sh
+++ test-scripts/test_PSA_02.sh
@@ -1,88 +1,154 @@
 #!/bin/bash
 
-# DOCS
-#
-# This script requires apt-get install dnsutils
+# This script requires:
+# apt-get install dnsutils
 # apt-get install nmap
 # apt-get install wget
 
-# Variables
+# ---------------------------------------
+# Constants
+# ---------------------------------------
+
 COLOR_RED=$'\033[0;31m'
 COLOR_GREEN=$'\033[0;32m'
+COLOR_YELLOW=$'\033[0;33m'
+COLOR_GRAY=$'\033[0;90m'
+COLOR_BLUE_BOLD=$'\033[4;94m'
+COLOR_GREY_BG=$'\033[0;30;47m'
 COLOR_RESET=$'\033[0m' # No Color
 
-echo "Start networking test script..."
+counter_passed=0
+counter_failed=0
 
-# Ping to internal team virtual machines
-team_vm_ip=192.168.8.2
+# ---------------------------------------
+# Test Boilerplate
+# ---------------------------------------
 
-if ping -c 1 $team_vm_ip &> /dev/null
-then
-  echo ${COLOR_GREEN}$team_vm_ip is reachable${COLOR_RESET}
-else
-  echo ${COLOR_RED}$team_vm_ip is not reachable${COLOR_RESET}
-fi
+print_title() {
+        echo ""
+        echo "${COLOR_GREY_BG}                                                                           ${COLOR_RESET}"
+        printf "${COLOR_GREY_BG}   %-72s${COLOR_RESET}\n" "$1"
+        echo "${COLOR_GREY_BG}                                                                           ${COLOR_RESET}"
+        echo ""
+}
 
-# Ping to external team virtual machines
-VM_IPs=(192.168.81.1 192.168.82.2 192.168.83.3 192.168.84.4 192.168.85.5 192.168.86.6 192.168.87.7 192.168.98.9 192.168.108.10)
-for ip in ${VM_IPs[@]}
-do
-        #echo Test reachability of VM $ip
-        if ping -c 1 $ip &> /dev/null
-        then
-                echo $ip is reachable
+print_headline() {
+        echo ""
+        echo " ${COLOR_BLUE_BOLD}$1${COLOR_RESET}"
+        echo ""
+}
+
+print_output () {
+        if [ $2 = true ] ; then
+                echo " [${COLOR_GREEN}PASS${COLOR_RESET}] ${COLOR_GRAY}$1${COLOR_RESET}"
+                ((counter_passed=counter_passed+1))
         else
-                echo $ip is not reachable
+                echo " [${COLOR_RED}FAIL${COLOR_RESET}] ${COLOR_GRAY}$1${COLOR_RESET}"
+                ((counter_failed=counter_failed+1))
         fi
-done
+}
 
-# Test website reachability
-output=$(wget -qO- http://tum.de)
+print_warning () {
+        echo " ${COLOR_YELLOW}Warning: $1${COLOR_RESET}"
+}
 
-if [ ${#output} -le 50 ]
-then
-        echo "error: tum.de not reachable"
+print_summary () {
+        echo ""
+        echo "${COLOR_GRAY}===========================================================================${COLOR_RESET}"
+        echo " Test Summary:"
+        echo "    ${COLOR_GREEN}TESTS PASSED: $counter_passed${COLOR_RESET}"
+        echo "    ${COLOR_RED}TESTS FAILED: $counter_failed${COLOR_RESET}"
+        echo ""
+}
+
+test_service_active () {
+        STATUS="$(systemctl is-active $1)"
+        if [ "${STATUS}" = "active" ]; then
+                print_output "Service $1 is running" true
+        else
+                print_output "Service $1 is not running" false
+        fi
+}
+
+# ---------------------------------------
+# Test Cases
+# ---------------------------------------
+
+print_title "Task 2: Network Test Script"
+
+if [ "$HOSTNAME" = "vmpsa08-02" ]; then
+		print_headline "Ping to internal team virtual machines"
+		
+		VM_IPs=(192.168.8.1 192.168.8.2 192.168.8.3 192.168.8.4 192.168.8.5 192.168.8.6)
+		for ip in ${VM_IPs[@]}
+		do
+				if ping -c 1 $ip &> /dev/null
+				then
+						print_output "VM ${ip} is reachable" true
+				else
+						print_output "VM ${ip} is not reachable" false
+				fi
+		done
+
+		print_headline "Ping to external team virtual machines"
+		
+		VM_IPs=(192.168.81.1 192.168.82.2 192.168.83.3 192.168.84.4 192.168.85.5 192.168.86.6 192.168.87.7 192.168.98.9 192.168.108.10)
+		for ip in ${VM_IPs[@]}
+		do
+				if ping -c 1 $ip &> /dev/null
+				then
+						print_output "VM ${ip} is reachable" true
+				else
+						print_output "VM ${ip} is not reachable" false
+				fi
+		done
+		
+		# Test DNS
+
+		if nslookup www.tum.de 2>&1 | grep -w "129.187.255.109" > /dev/null ; then
+			print_output "Our domain can be resolved" true
+		else
+			print_output "Our domain can not be resolved" false
+		fi
+		
+		print_headline "Test external IP address"
+		
+		external_ip=srv1.ulinky.de
+		if ping -c 1 $external_ip &> /dev/null
+		then
+			print_output "External ip be resolved" true
+		else
+			print_output "External ip can not be resolved" false
+		fi
+		
+		print_headline "Test FMI building network"
+
+		ip="lxhalle.in.tum.de"
+		res=$(nmap ${ip} -PN -p ssh | grep open)
+
+		# -- if result contains open, we can reach ssh else assume failure) --
+		if [[ "${res}" =~ "open" ]] ;then
+			print_output "It's Open! Let's SSH to it.." true
+		else
+			print_output "The host ${ip} is not accessible!" false
+		fi
+		
+		print_headline "Test website reachability"
+		output=$(wget -qO- http://tum.de)
+
+		if [ ${#output} -le 50 ]
+		then
+			print_output "Website tum.de is not reachable" false
+		else
+			print_output "Website tum.de is reachable" true
+		fi
+		
+		print_headline "Port scanning for 192.168.8.2"
+		nmap 192.168.8.2
+		nmap -sU 192.168.8.2
 else
-        echo "tum.de is reachable"
+        print_warning "Netzwerk Tests bitte auf Webserver (VM2) ausfuehren!"
 fi
 
+print_summary
 
-# Test DNS
-
-# Checking for the resolved IP address from the end of the command output. Refer
-# the normal command output of nslookup to understand why.
-
-DNS_LOOKUP_DOMAIN=www.tum.de
-resolvedIP=$(nslookup $DNS_LOOKUP_DOMAIN | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
-
-# Deciding the lookup status by checking the variable has a valid IP string
-
-[[ -z "$resolvedIP" ]] && echo "$DNS_LOOKUP_DOMAIN" lookup failure || echo "$DNS_LOOKUP_DOMAIN" resolved to "$resolvedIP"
-
-
-# Test external IP address
-external_ip=srv1.ulinky.de
-
-if ping -c 1 $external_ip &> /dev/null
-then
-  echo ${COLOR_GREEN}$external_ip is reachable${COLOR_RESET}
-else
-  echo ${COLOR_RED}$external_ip is not reachable${COLOR_RESET}
-fi
-
-# Test FMI building network
-
-ip="lxhalle.in.tum.de"
-res=$(nmap ${ip} -PN -p ssh | grep open)
-
-# -- if result contains open, we can reach ssh else assume failure) --
-if [[ "${res}" =~ "open" ]] ;then
-    echo "It's Open! Let's SSH to it.."
-else
-    echo "The host ${ip} is not accessible!"
-fi
-
-
-# Port scanning
-nmap 192.168.8.2
-nmap -sU 192.168.8.2
--- test-scripts/test_PSA_03.sh
+++ test-scripts/test_PSA_03.sh
@@ -1,74 +1,134 @@
+#!/bin/bash
 
-!/bin/bash
+# apt-get install curl lynx
 
-echo -e "This is our test script for exercise 3 (DNS and DCHP)\n"
+# ---------------------------------------
+# Constants
+# ---------------------------------------
 
-# DNS Lookup
+COLOR_RED=$'\033[0;31m'
+COLOR_GREEN=$'\033[0;32m'
+COLOR_YELLOW=$'\033[0;33m'
+COLOR_GRAY=$'\033[0;90m'
+COLOR_BLUE_BOLD=$'\033[4;94m'
+COLOR_GREY_BG=$'\033[0;30;47m'
+COLOR_RESET=$'\033[0m' # No Color
 
-# Test that other domains can be resolved
+counter_passed=0
+counter_failed=0
 
-echo -e "\nCheck if a random domain can be resolved:\n"
+# ---------------------------------------
+# Test Boilerplate
+# ---------------------------------------
 
-DNS_LOOKUP_DOMAIN=www.tum.de
+print_title() {
+        echo ""
+        echo "${COLOR_GREY_BG}                                                                           ${COLOR_RESET}"
+        printf "${COLOR_GREY_BG}   %-72s${COLOR_RESET}\n" "$1"
+        echo "${COLOR_GREY_BG}                                                                           ${COLOR_RESET}"
+        echo ""
+}
 
-resolvedIP=$(nslookup $DNS_LOOKUP_DOMAIN | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
+print_headline() {
+        echo ""
+        echo " ${COLOR_BLUE_BOLD}$1${COLOR_RESET}"
+        echo ""
+}
 
-[[ -z "$resolvedIP" ]] && echo "$DNS_LOOKUP_DOMAIN" lookup failure || echo "$DNS_LOOKUP_DOMAIN" resolved to "$resolved>
+print_output () {
+        if [ $2 = true ] ; then
+                echo " [${COLOR_GREEN}PASS${COLOR_RESET}] ${COLOR_GRAY}$1${COLOR_RESET}"
+                ((counter_passed=counter_passed+1))
+        else
+                echo " [${COLOR_RED}FAIL${COLOR_RESET}] ${COLOR_GRAY}$1${COLOR_RESET}"
+                ((counter_failed=counter_failed+1))
+        fi
+}
+
+print_warning () {
+        echo " ${COLOR_YELLOW}Warning: $1${COLOR_RESET}"
+}
+
+print_summary () {
+        echo ""
+        echo "${COLOR_GRAY}===========================================================================${COLOR_RESET}"
+        echo " Test Summary:"
+        echo "    ${COLOR_GREEN}TESTS PASSED: $counter_passed${COLOR_RESET}"
+        echo "    ${COLOR_RED}TESTS FAILED: $counter_failed${COLOR_RESET}"
+        echo ""
+}
+
+test_service_active () {
+        STATUS="$(systemctl is-active $1)"
+        if [ "${STATUS}" = "active" ]; then
+                print_output "Service $1 is running" true
+        else
+                print_output "Service $1 is not running" false
+        fi
+}
+
+# ---------------------------------------
+# Test Cases
+# ---------------------------------------
+
+print_title "Task 3: DNS and DHCP Test Script"
+
+if [ "$HOSTNAME" = "vmpsa08-02" ]; then
+        print_headline "Testing DNS"
+
+		if nslookup www.tum.de 2>&1 | grep -w "129.187.255.109" > /dev/null ; then
+			print_output "Webserver is reachable via HTTP" true
+		else
+			print_output "Webserver is not reachable via HTTP" false
+		fi
+		
+		if dig @192.168.8.1 psa-team8.cit.tum.de 2>&1 | grep -w " 192.168.8.1" > /dev/null ; then
+			print_output "Our domain can be resolved" true
+		else
+			print_output "Our domain can not be resolved" false
+		fi
+
+		if dig @192.168.8.1 -x 192.168.8.1 2>&1 | grep -w "vm1.psa-team8.cit.tum.de." > /dev/null ; then
+			print_output "Reverse lookup working for 192.168.8.1" true
+		else
+			print_output "Reverse lookup working not working for 192.168.8.1" false
+		fi
 
 
-# Test that we can resolve our domain
+		if dig @192.168.8.1 psa-team6.cit.tum.de 2>&1 | grep -w "hostmaster.psa-team6.cit.tum.de." > /dev/null ; then
+			print_output "Domain psa-team6.cit.tum.de can be resolved" true
+		else
+			print_output "Domain psa-team6.cit.tum.de can not be resolved" false
+		fi
 
-echo -e "\nCheck if we can resolve our domain:\n"
+		if dig @192.168.86.6 psa-team8.cit.tum.de 2>&1 | grep -w "192.168.8.1" > /dev/null ; then
+			print_output "Team 6 DNS serves as secondary nameserver" true
+		else
+			print_output "Team 6 DNS serves not as secondary nameserver" false
+		fi
+		
+		print_headline "Ping DHCP Server"
+		
+		if /usr/sbin/dhcping -s 192.168.8.1 2>&1 | grep -w "Got answer from: 192.168.8.1" > /dev/null ; then
+			print_output "DHCP Server is responding" true
+		else
+			print_output "DHCP Server is not responding" false
+		fi
+		
+		print_headline "Check leases file for information sent by dhcp server"
+		
+		sed '17!d' /var/lib/dhcp/dhclient.leases
+		sed '18!d' /var/lib/dhcp/dhclient.leases
+		sed '19!d' /var/lib/dhcp/dhclient.leases
+		sed '20!d' /var/lib/dhcp/dhclient.leases
+		sed '21!d' /var/lib/dhcp/dhclient.leases
+		sed '22!d' /var/lib/dhcp/dhclient.leases
+		sed '23!d' /var/lib/dhcp/dhclient.leases
+		sed '24!d' /var/lib/dhcp/dhclient.leases
+		sed '25!d' /var/lib/dhcp/dhclient.leases
+		sed '26!d' /var/lib/dhcp/dhclient.leases
+else
+        print_warning "DNS und DHCP Tests bitte auf Webserver (VM2) ausfuehren!"
+fi
 
-dig @192.168.8.1 psa-team8.cit.tum.de
-
-
-# Test reverse lookup
-
-echo -e "\nCheck reserve lookup:\n"
-
-dig @192.168.8.1 -x 192.168.8.1
-
-
-# Test that domain of other team can be resolved
-cho "Check if domain of team 6 can be resolved:"
-
-dig @192.168.8.1 psa-team6.cit.tum.de
-
-
-# Check that another server works as secondary nameserver
-
-echo "Check if team 6 serves as secondary nameserver:"
-
-dig @192.168.86.6 psa-team8.cit.tum.de
-
-
-
-# DCHP
-
-# Ping DHCP Server
-
-echo -e "\nPing our DHCP-Server\n"
-
-dhcping -s 192.168.8.1
-
-#
-
-#read /var/lib/dhcp/dhclient.leases
-#while read line
-#do echo $line
-#done < /var/lib/dhcp/dhclient.leases
-
-echo -e "\nCheck leases file for information sent by DHCP-Server:\n"
-
-sed '17!d' /var/lib/dhcp/dhclient.leases
-sed '18!d' /var/lib/dhcp/dhclient.leases
-sed '19!d' /var/lib/dhcp/dhclient.leases
-sed '20!d' /var/lib/dhcp/dhclient.leases
-sed '21!d' /var/lib/dhcp/dhclient.leases
-sed '22!d' /var/lib/dhcp/dhclient.leases
-sed '23!d' /var/lib/dhcp/dhclient.leases
-sed '24!d' /var/lib/dhcp/dhclient.leases
-sed '25!d' /var/lib/dhcp/dhclient.leases
-sed '26!d' /var/lib/dhcp/dhclient.leases
-
+print_summary
